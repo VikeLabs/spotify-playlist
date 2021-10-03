6212d0f7afeb9204affa771f084a9efc9f1b00ab
Poomon001 <57118681+Poomon001@users.noreply.github.com>
2021-10-02 18:57:32 -0700

    add fetch request code from queryString

--- server/controllers/hello.js
+++ server/controllers/hello.js
@@ -1,5 +1,11 @@
+const { request } = require("express");
+var querystring = require("querystring"); // querystring module
+
 // a route handler
 const greeting = (req, res) => {
+  var code = req.query.code || null;
+
+  console.log(code);
   res.json({
     someText: ["poomon", "hello", "world"],
   });
