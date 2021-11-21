0b5f1d5d6be711860b5de6328d2eac95a948bbeb
Poomon001 <57118681+Poomon001@users.noreply.github.com>
2021-11-20 19:07:25 -0800

    fix fetching data from Spotify API issue + add bootstrap react to frontend

--- client/app/src/App.js
+++ client/app/src/App.js
@@ -1,10 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import { TestUi } from "../src/components/TestUi";
+import Content from "./components/Content";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <TestUi />
-      <h1>hello</h1>
+      <NavBar />
+      <Content />
     </div>
   );
 }
---
+++ client/app/src/components/Content.js
@@ -0,0 +1,11 @@
+import Button from "react-bootstrap/Button";
+const Content = () => {
+  return (
+    <>
+      <Button>Start</Button>
+      <div>Body start here</div>
+    </>
+  );
+};
+
+export default Content;
